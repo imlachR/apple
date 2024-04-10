47b44e5f091f01fc912bae05d4f99dcd23d9a9dd
imlachR <rodrimlach@gmail.com>
2024-04-10 15:02:01 -0300

    Introduce main components (Model, Features, HowItWorks, Footer) along with their sub-components and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Highlights from "./components/Highlights";
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 const App = () => {
-
   return (
     <main className="bg-black">
       <Navbar />
-      <Hero /> 
+      <Hero />
       <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
 
-export default App
+export default App;
